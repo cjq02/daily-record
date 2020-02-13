b50e522ed345bcd7d2de2817b4f56efef55b3994
jq_chen <jq_chen@onlyou.com>
2020-02-14 00:43:47 +0800

    Complete Record Config

--- dr-server/src/modules/record/controller/record.config.controller.ts
+++ dr-server/src/modules/record/controller/record.config.controller.ts
@@ -28,7 +28,7 @@ export class RecordConfigController {
     }
 
     @Delete(':id')
-    remove(@Param('id') id: string) {
+    delete(@Param('id') id: string) {
         return this.recordConfigService.delete(id);
     }
 }
